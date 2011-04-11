8cd40299d624c2e5db14a03f731d17a6035ce810
nander <nanderv@gmail.com>
2011-04-11 22:04:15 +0200

    Added info about starting parameters

--- main.cpp
+++ main.cpp
@@ -50,10 +50,11 @@ return now;
 
 stamp calcnextStepNan(stamp now, double timestep){
     pos p_now; // a position variable // V
+    double aa = now.a2;
     double vg= sin(now.a2)*sqrt(G/now.l2);// V
-now.v2+=vg*timestep; // V
+now.v2-=vg*timestep; // V
 now.v2 -= now.v2*timestep*now.damp;// V
-now.a2=now.a2-now.v2*timestep; // V
+now.a2=now.a2+now.v2*timestep; // V
 p_now.x2=now.l1*sin(now.a1)+now.l2*sin(now.a2); // Old position of the middle + new position of the end V
 p_now.y2=now.l1*cos(now.a1)+now.l2*cos(now.a2); // V
 
@@ -63,9 +64,7 @@ now.a1+=now.v1*timestep; // move point 1 // V
 p_now.x1=now.l1*sin(now.a1); // V
 p_now.y1=now.l1*cos(now.a1); // V
 double new_angle=atan((p_now.x2-p_now.x1)/(p_now.y2-p_now.y1)); // calculate the new angle
-
-
-now.v2=(new_angle-now.a2)/timestep; // calculate the angular velocity .. This requires attention
+now.v2 = ((new_angle-now.a2)/timestep)+aa-now.a2;// calculate the angular velocity .. This requires attention
 
 return now;
 }
@@ -85,21 +84,21 @@ int main()
 
     sf::RenderWindow App(sf::VideoMode(800, 600), "Numeriek model");
     stamp now;
-    now.a1=0;
-    now.damp=0.0000;
-    now.a2=0*pi;
-    now.v1=4*2*pi;
-    now.v2=0;
-bool nan=true;
-bool dif=true;
-now.l1 = 0.02;
-now.l2=0.10;
-float rate=200;
-float timespeed=1;
-bool clear=true;
-double scale = 200/(now.l1+now.l2);
-double timestep = 0.002;
-stamp now2=now;
+    now.a1=0; // Starting angle 1
+    now.damp=0.0000; // Damping value
+    now.a2=pi;  // Starting angle 2
+    now.v1=4*2*pi;// (starting) speed 1
+    now.v2=0; // (starting) speed 2
+bool nan=true; // Does Nanders Algorithm run?
+bool dif=true; // Does the differential equotation run?
+now.l1 = 0.02; // Length 1
+now.l2=0.10; // Length 2
+float rate=200; // Frames per sedond
+float timespeed=10; // Slowdown (10 = ten times slower then normal)
+bool clear=true; // Clear the screen?
+double scale = 200/(now.l1+now.l2); // Calculate screen scale
+double timestep = 0.000002; // Number of calculated seconds per frame.
+stamp now2=now; // Second now (so each model starts with the same starting parameters)
 
     while (App.IsOpened())
     {
